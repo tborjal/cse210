8cad8e9636999f22a7e40d9c70c569b280fd3230
tborjal <bor22014@byui.edu>
2023-11-04 00:07:20 +0800

    adjusted to random number generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,9 +5,10 @@ class Program
     static void Main(string[] args)
     {
        
-        Console.WriteLine("What is your magic number:?");
-        int magicnumber = int.Parse(Console.ReadLine());
-
+        // Console.WriteLine("What is your magic number:?");
+        // int magicnumber = int.Parse(Console.ReadLine());
+        Random randomnumber = new Random();
+        int magicnumber = randomnumber.Next(1,101);
         int guess = 0;
 
         while (guess != magicnumber){
