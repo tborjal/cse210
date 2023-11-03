c00b541decaa5c1d5e4d9e4d3e2b73aa9f07999f
tborjal <bor22014@byui.edu>
2023-11-03 20:01:02 +0800

    added if else statements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
 
-class Program
+Console.WriteLine("Hello Prep2 World!");
+
+Console.WriteLine("What is your grade?:");
+string input = Console.ReadLine();
+int grade = int.Parse(input);
+
+
+string letter = "";
+
+    if (grade >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+       letter = "A";
 }
+    else if (grade >= 80)
+{
+        letter = "B";
+}
+    else if (grade >= 70)
+{
+        letter = "C";
+}
+    else if (grade >= 60)
+{
+        letter = "D";
+}
+    else if (grade < 60)
+{
+       letter = "F";
+}
+    else
+{
+        Console.WriteLine("Error. Please try again");
+}
+
+    Console.WriteLine($"Your grade is: {letter} ");
+
+    if (grade >= 70){
+        Console.WriteLine("Good Job! You passed.");
+    }
+    else if(grade <=69){
+
+        Console.WriteLine("You can still try again. Better luck next time!");
+    }
+    else {
+        Console.Write("Error, Please try again.");
+    }
+
+
