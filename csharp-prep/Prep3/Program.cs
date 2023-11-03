b7a786f651540168eac6ab7c6a89bc548edafe41
tborjal <bor22014@byui.edu>
2023-11-04 00:03:42 +0800

    debugged for loop code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+        Console.WriteLine("What is your magic number:?");
+        int magicnumber = int.Parse(Console.ReadLine());
+
+        int guess = 0;
+
+        while (guess != magicnumber){
+
+            Console.WriteLine("What is your guess?:");
+            guess = int.Parse(Console.ReadLine());
+
+            if(guess > magicnumber){
+                Console.WriteLine("Lower");
+            }
+            else if(guess < magicnumber){
+                Console.WriteLine("Higher");
+            }
+            else if(guess == magicnumber){
+                Console.WriteLine("You guessed it.");
+            }
+            else {
+                Console.WriteLine("Error. Please try again.");
+            }
+        }
+        
     }
 }
