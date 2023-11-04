f49a559ab04a3f91fc101bbdfaaf1a673db3d319
tborjal <bor22014@byui.edu>
2023-11-04 10:37:53 +0800

    adjusted code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,47 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+    Console.WriteLine("Hello Prep5 World!");
 
-        DisplayWelcomeMessage();
+    DisplayWelcomeMessage();
 
-        string userName = PromptUserName();
-        int userNumber = PromptUserNumber();
+    string userName = PromptUserName();
+    int userNumber = PromptUserNumber();
 
-        int squaredNumber = SquareNumber(userNumber);
+    int squaredNumber = SquareNumber(userNumber);
 
-        DisplayResult(userName, squaredNumber);
+    DisplayResult(userName, squaredNumber);
     }
+
     static void DisplayWelcomeMessage()
     {
-        Console.WriteLine("Welcome to the program!");
+    Console.WriteLine("Welcome to the program!");
     }
 
     static string PromptUserName()
     {
-        Console.Write("Please enter your name: ");
-        string name = Console.ReadLine();
+    Console.Write("Please enter your name: ");
+    Console.Out.Flush(); // Flush the output to ensure prompt is displayed immediately
+    string name = Console.ReadLine();
 
-        return name;
+    return name;
     }
 
     static int PromptUserNumber()
     {
-        Console.Write("Please enter your favorite number: ");
-        int number = int.Parse(Console.ReadLine());
+    Console.Write("Please enter your favorite number: ");
+    Console.Out.Flush(); // Flush the output to ensure prompt is displayed immediately
+    int number = int.Parse(Console.ReadLine());
 
-        return number;
+    return number;
     }
 
     static int SquareNumber(int number)
     {
-        int square = number * number;
-        return square;
+    int square = number * number;
+    return square;
     }
 
     static void DisplayResult(string name, int square)
     {
-        Console.WriteLine($"{name}, the square of your number is {square}");
+    Console.WriteLine($"Brother {name}, the square of your number is {square}");
     }
 
 
